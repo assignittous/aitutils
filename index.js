411311c88fb46241eb6648431cb9cc58ec45ec85
Steven Ng <steven.ng@braintapper.com>
2015-07-21 16:25:19 -0400

    Compile lib

---
+++ index.js
@@ -0,0 +1,8 @@
+exports.aitutils = {
+  array: require("./lib/array").array,
+  data: require("./lib/data").data,
+  file: require("./lib/file").file,
+  general: require("./lib/general").general,
+  logger: require("./lib/logger").logger,
+  xml: require("./lib/xml").xml
+};
