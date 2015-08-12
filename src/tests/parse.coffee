3a3a9b3b091c75e5c3e96cdfa709fcfcfe103587
Steven Ng <steven.ng@braintapper.com>
2015-08-12 15:24:59 -0400

    Prewiring some automated tests for parse

--- src/tests/parse.coffee
+++ src/tests/parse.coffee
@@ -1,1 +1,37 @@
-# parse.coffee
+logger = require("../lib/logger").logger
+cson = require("cson")
+
+require "sugar"
+###
+logger.custom "test message",
+  type: "test"
+  background: "Yellow"
+  foreground: "black"
+###
+
+parse = require("../lib/parse").parse
+
+testStrings = cson.parseCSONFile("tests/parse_strings.cson")
+
+compare = (a,b)->
+  if (a.length == 0) & (b.length == 0)
+    return true
+  else
+    return (a.intersect(b).length == a.length)
+
+testStrings.each (testString)->
+  
+  logger.info testString.text
+
+  relativeDates = parse.relativeDate(testString.text)
+
+  if compare(testString.relativeDates, relativeDates)
+    logger.pass "Relative Dates"
+  else
+    logger.fail "Relative Dates"
+  console.log "Expected: [#{testString.relativeDates.join(',')}]"
+  console.log "Got: [#{relativeDates.join(',')}]"
+
+
+  #console.log testString
+  
--- tests/parse.js
+++ tests/parse.js
@@ -1,1 +1,40 @@
+var compare, cson, logger, parse, testStrings;
 
+logger = require("../lib/logger").logger;
+
+cson = require("cson");
+
+require("sugar");
+
+
+/*
+logger.custom "test message",
+  type: "test"
+  background: "Yellow"
+  foreground: "black"
+ */
+
+parse = require("../lib/parse").parse;
+
+testStrings = cson.parseCSONFile("tests/parse_strings.cson");
+
+compare = function(a, b) {
+  if ((a.length === 0) & (b.length === 0)) {
+    return true;
+  } else {
+    return a.intersect(b).length === a.length;
+  }
+};
+
+testStrings.each(function(testString) {
+  var relativeDates;
+  logger.info(testString.text);
+  relativeDates = parse.relativeDate(testString.text);
+  if (compare(testString.relativeDates, relativeDates)) {
+    logger.pass("Relative Dates");
+  } else {
+    logger.fail("Relative Dates");
+  }
+  console.log("Expected: [" + (testString.relativeDates.join(',')) + "]");
+  return console.log("Got: [" + (relativeDates.join(',')) + "]");
+});
