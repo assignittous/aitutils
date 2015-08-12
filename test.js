d4471608f3ff8b5129a5c36cf9d949f6d8fa1292
Steven Ng <steven.ng@braintapper.com>
2015-08-12 14:31:23 -0400

    prewire automated tests

--- test.js
+++ test.js
@@ -1,9 +1,23 @@
-var logger;
+#!/usr/bin/env node
+var _, logger, parse, testStrings;
 
 logger = require("./lib/logger").logger;
 
-logger.custom("test message", {
-  type: "test",
-  background: "Yellow",
+_ = require("lodash");
+
+
+/*
+logger.custom "test message",
+  type: "test"
+  background: "Yellow"
   foreground: "black"
+ */
+
+parse = require("./lib/parse").parse;
+
+testStrings = ["send me this year's income statement", "give me last quarter's income statement", "tell me the income statement for last year", "show me the income statement for last month", "send me last week's status report", "what were sales for the month of july", "what were sales for 07/2015", "how many cars were sold in july", "how many tires were sold in july, 2014", "how many tires were sold last july", "last month's income statement", "tires sold in july 2014", "Send me yesterday's sales report", "What were last quarter's sales for widgets", "I need today's dashboard"];
+
+_.forEach(testStrings, function(testString) {
+  logger.info(testString);
+  return console.log(parse.relativeDate(testString));
 });
